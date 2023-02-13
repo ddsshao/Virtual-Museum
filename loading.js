7d4f102a400d6a22cf2f9b6e30d0738fb8d41369
ddsshao <107378731+ddsshao@users.noreply.github.com>
2023-02-13 12:42:23 -0800

    optimization

--- loading.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var loading = document.querySelector('#loading');
-    var scene = document.querySelector('a-scene');
-  
-    scene.addEventListener('loaded', function (e) {
-      // hide the loading screen
-      loading.style.display = 'none';
-    });
-  });
